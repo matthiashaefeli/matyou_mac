b1c8e1bec62d81011e014b121ec82c54e464c6f7
matthias haefeli <haefeli.matthias@gmail.com>
2020-08-11 14:28:46 -0600

    add system disk view, wheel

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/home/terminal', to: 'home#terminal'
   get '/home/trash', to: 'home#trash'
+  get '/home/system_disk', to: 'home#system_disk'
   devise_for :users
   resources :notes
   resources :blogs
